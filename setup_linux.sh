72286d36490d502ce4d369a086cde8d76acb145e
EmilPi <emilis.pileckis@gmail.com>
2022-02-02 10:44:51 +0200

    Simplify + add missing requirements

---
+++ setup_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install -y xdotool scrot python3-tk python3-dev
+python3 -m pip install -r requirements_linux.txt
+# if screenshot grabbing fails
+# sudo apt install python3-pil
